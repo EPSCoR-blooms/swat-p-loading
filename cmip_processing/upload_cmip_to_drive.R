87a55604ffaeae6964c1c4362395b85ce9e4ac39
B Steele <steeleb@caryinstitute.org>
2022-10-19 17:37:05 -0600

    debug upload

--- cmip_processing/upload_cmip_to_drive.R
+++ cmip_processing/upload_cmip_to_drive.R
@@ -2,9 +2,12 @@
 upload <- list.files('toupload')
 
 for(u in 1:length(upload)){
-  drive_mkdir(lake_list$LakeName[l], path = as_id(inter_id))
-  lid = drive_ls(as_id(inter_id), pattern = lake_list$LakeName[l])
-  drive_upload(upload[u], path = lid)
+  lid <- drive_ls(as_id(inter_id), pattern = lake_list$LakeName[l])
+  if(nrow(lid) == 0){
+    drive_mkdir(lake_list$LakeName[l], path = as_id(inter_id))
+    lid = drive_ls(as_id(inter_id), pattern = lake_list$LakeName[l])
+    } else {}
+  drive_upload(file.path('toupload',upload[u]), path = lid$id, overwrite = T)
 }
 
 #delete folder of files to upload
